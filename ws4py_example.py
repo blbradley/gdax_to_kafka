c4a78b9fb7771e839ca4edee53a483a807ea8563
Brandon Bradley <bradleytastic@gmail.com>
2017-07-21 13:04:57 -0500

    create producer uuid, use as Kafka message key (#1)

--- ws4py_example.py
+++ ws4py_example.py
@@ -1,6 +1,10 @@
+import uuid
+
 from ws4py.client.threadedclient import WebSocketClient
 
 from myproducer import producer
+
+producer_uuid = uuid.uuid4()
 
 class DummyClient(WebSocketClient):
     def opened(self):
@@ -14,7 +18,7 @@ class DummyClient(WebSocketClient):
         print("Closed down", code, reason)
 
     def received_message(self, m):
-        future = producer.send('gdax', str(m).encode('utf-8'))
+        future = producer.send('gdax', str(m).encode('utf-8'), producer_uuid.bytes)
         result = future.get(timeout=10)
 
 if __name__ == '__main__':
