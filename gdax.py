a064ae0792176a9417da1e114dd77aa061e88ec0
Brandon Bradley <bradleytastic@gmail.com>
2017-09-09 14:30:29 -0500

    [WIP] flat schema for raw data using Avro unions

--- gdax.py
+++ gdax.py
@@ -1,5 +1,7 @@
+import json
 from myproducer import producer
 
 def create_raw(dt, data):
-    raw = {'timestamp': str(dt), 'producerUUID': producer.uuid.bytes, 'data': data}
+    extra = {'time_collected': str(dt), 'producerUUID': producer.uuid.bytes}
+    raw = {**json.loads(data), **extra}
     return raw
