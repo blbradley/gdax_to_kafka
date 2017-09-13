b6429e0b8c1590fcd7652d8a35591b5a013bc27a
Brandon Bradley <bradleytastic@gmail.com>
2017-09-13 16:03:35 -0500

    write to Kafka with string schemas

--- polling.py
+++ polling.py
@@ -1,17 +1,32 @@
 import logging
+import json
 
 from apscheduler.schedulers.background import BlockingScheduler
+from confluent_kafka import avro
 import requests
+
+from myproducer import producer
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 sched = BlockingScheduler()
 
+key_schema = avro.loads(json.dumps({'type': 'string'}))
+
 def my_job():
     logging.warning('foo')
     r = requests.get('https://api.gdax.com/products/BTC-USD/ticker')
     print(r.json())
+    value = str(r.json())
+    producer.produce(
+        topic='gdax-polling',
+        value=value,
+        key=str(producer.uuid),
+        key_schema=key_schema,
+        value_schema=key_schema,
+    )
+    producer.poll(0)
 
 sched.add_job(my_job, 'interval', seconds=1)
 sched.start()
