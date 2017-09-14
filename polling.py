a933125758ae11b0f9a5de43ee9f09c2c1d5b11b
Brandon Bradley <bradleytastic@gmail.com>
2017-09-14 13:37:45 -0500

    try out processpool

--- polling.py
+++ polling.py
@@ -5,18 +5,16 @@ from apscheduler.schedulers.background import BlockingScheduler
 from confluent_kafka import avro
 import requests
 
-from myproducer import producer
-
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-sched = BlockingScheduler()
 
 key_schema = avro.loads(json.dumps({'type': 'string'}))
 
 def produce_ticker():
+    from myproducer import producer
     r = requests.get('https://api.gdax.com/products/BTC-USD/ticker')
     value = str(r.json())
+    print(value)
     producer.produce(
         topic='gdax-polling-ticker',
         value=value,
@@ -27,8 +25,10 @@ def produce_ticker():
     producer.poll(0)
 
 def produce_trades():
+    from myproducer import producer
     r = requests.get('https://api.gdax.com/products/BTC-USD/trades')
     value = str(r.json())
+    print(value)
     producer.produce(
         topic='gdax-polling-trades',
         value=value,
@@ -38,7 +38,13 @@ def produce_trades():
     )
     producer.poll(0)
 
+if __name__ == '__main__':
+    sched = BlockingScheduler()
+    sched.add_executor('processpool')
+    sched.add_job(produce_ticker, 'interval', seconds=1)
+    sched.add_job(produce_trades, 'interval', seconds=1)
 
-sched.add_job(produce_ticker, 'interval', seconds=2)
-sched.add_job(produce_trades, 'interval', seconds=2)
-sched.start()
+    try:
+        sched.start()
+    except KeyboardInterrupt:
+        pass
