95ac159593cb17540b6432be8791a35a636346ba
Brandon Bradley <bradleytastic@gmail.com>
2017-09-14 14:12:08 -0500

    add order book collection

--- polling.py
+++ polling.py
@@ -10,11 +10,23 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 key_schema = avro.loads(json.dumps({'type': 'string'}))
 
+def produce_book():
+    from myproducer import producer
+    r = requests.get('https://api.gdax.com/products/BTC-USD/book?level=3')
+    value = str(r.json())
+    producer.produce(
+        topic='gdax-polling-book',
+        value=value,
+        key=str(producer.uuid),
+        key_schema=key_schema,
+        value_schema=key_schema,
+    )
+    producer.poll(0)
+
 def produce_ticker():
     from myproducer import producer
     r = requests.get('https://api.gdax.com/products/BTC-USD/ticker')
     value = str(r.json())
-    print(value)
     producer.produce(
         topic='gdax-polling-ticker',
         value=value,
@@ -28,7 +40,6 @@ def produce_trades():
     from myproducer import producer
     r = requests.get('https://api.gdax.com/products/BTC-USD/trades')
     value = str(r.json())
-    print(value)
     producer.produce(
         topic='gdax-polling-trades',
         value=value,
@@ -41,6 +52,7 @@ def produce_trades():
 if __name__ == '__main__':
     sched = BlockingScheduler()
     sched.add_executor('processpool')
+    sched.add_job(produce_book, 'interval', seconds=5)
     sched.add_job(produce_ticker, 'interval', seconds=1)
     sched.add_job(produce_trades, 'interval', seconds=1)
 
